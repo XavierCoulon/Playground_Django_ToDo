b3d5025558400580f6c2d70b63f5f8db77269b0b
XavierCoulon <xcoulon.mail@gmail.com>
2022-07-21 15:03:34 +0200

    postgresql

--- src/todo/settings.py
+++ src/todo/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'todo',
         'USER': 'admin',
         'PASSWORD': 'admin',
-        'HOST': 'localhost',
+        'HOST': 'postgres',
         'PORT': '5430'
     }
 }
